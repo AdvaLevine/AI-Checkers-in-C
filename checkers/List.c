b36b0467fcfffab7ee446af33ac37c7525f40cc8
advalevine5798@gmail.com <advalevine5798@gmail.com>
2023-06-05 16:46:42 +0300

    עריכת main.c list.c

--- checkers/List.c
+++ checkers/List.c
@@ -3,69 +3,68 @@
 
 
 
-
+//This function gets a tree of one pos and creates an optimal move list
 SingleSourceMovesList* FindSingleSourceOptimalMove(SingleSourceMovesTree* moves_tree) {
 
-	SingleSourceMovesList* res;
-	unsigned short capturesOfRoute=0;// saving the max route
-	int row = (moves_tree->source->pos->row) - CHAR_A; //from letter to number
-	int col = (moves_tree->source->pos->col) - CHAR_0 - 1; //from char number to int number
-	char player = (moves_tree->source->board)[row][col];
-	//int level = 0;
-	//int arr[MAX_CAPTURES_FOR_SINGLE_MOVE] = { 0 };//maybe initilize
-	//int colsArr[MAX_CAPTURES_FOR_SINGLE_MOVE] = { 0 };
+	SingleSourceMovesList* res;									// the list
+	unsigned short capturesOfRoute=0;							// saving the max route
+	int row = (moves_tree->source->pos->row) - CHAR_A;			//convert from letter to number
+	int col = (moves_tree->source->pos->col) - CHAR_0 - 1;		//convert from char number to int number
+	char player = (moves_tree->source->board)[row][col];		//save the players char
+	//allocate the list
 	res = (SingleSourceMovesList*)malloc(sizeof(SingleSourceMovesList));
-	checkAllocationList(res);
-	/*makeEmptyList(&res);*/
-	
+	checkAllocationList(res);									//check if allocation was successful
+	makeEmptyList(res);											//put NULL in list
+
+	//get the optimal list
 	res->head = createOptimalListFromTree(moves_tree->source, player, &capturesOfRoute);
-	res->tail = getTail(res, capturesOfRoute);
+	res->tail = getTail(res, capturesOfRoute);					//update the tail
 
-	//maxRouteList(res,capturesOfRoute,arr)
-	
-	freeTree(moves_tree);
-	return res;
+	freeTree(moves_tree);										//free the tree
+	return res;													//return the list
 }
-
+//This function is creating the optimal list according to the rules of the game
 SingleSourceMovesListCell* createOptimalListFromTree(SingleSourceMovesTreeNode* source, char player, unsigned short* capturesOfRoute)
 {
-	unsigned short captureRouteLeft, captureRouteRight;
-	SingleSourceMovesListCell* root; //change to node
-	SingleSourceMovesListCell* leftListNode;
-	SingleSourceMovesListCell* rightListNode;
-	if (source == NULL) {//if empty tree return
+	unsigned short captureRouteLeft, captureRouteRight;			//number of captures from left and right
+	SingleSourceMovesListCell* root;							//the list head
+	SingleSourceMovesListCell* leftListNode;					//the left list
+	SingleSourceMovesListCell* rightListNode;					//the right list
+
+	if (source == NULL) {										//if empty tree return
 		*capturesOfRoute = 0;
 		return NULL;
 	}
-	root = createNewListCell(source->pos, source->total_captures_so_far, NULL);
-	//if its a leaf, we return the capture move in it
-	if ((source->next_move[LEFT] == NULL) && (source->next_move[RIGHT] == NULL)) {
+
+	root = createNewListCell(source->pos, source->total_captures_so_far, NULL);		//create a new list node
+	
+	if ((source->next_move[LEFT] == NULL) && (source->next_move[RIGHT] == NULL)) {	//if its a leaf, we return the capture move in it
 		*capturesOfRoute = source->total_captures_so_far;
 		return root;
 	}
-	
+	//recursive calls for lest and right lists
 	leftListNode = createOptimalListFromTree(source->next_move[LEFT], player, &captureRouteLeft);
 	rightListNode = createOptimalListFromTree(source->next_move[RIGHT], player, &captureRouteRight);
 
 	if (captureRouteRight == captureRouteLeft) {//same number of captures
 		//rule in page 2
 		if (player == PLAYER_T) {
-			if (rightListNode != NULL) {
+			if (rightListNode != NULL) {		//prioritizing right(higher col)
 				root->next = rightListNode;
-					freeRoute(leftListNode); //free the left route
+					freeRoute(leftListNode);	 //free the left route
 			}
 			else
 				root->next=leftListNode;
 		}
 		else {//player is B
-			if (leftListNode != NULL) {
+			if (leftListNode != NULL) {			//prioritizing left(lower col)
 				root->next = leftListNode;
-				freeRoute(rightListNode); //free the right route
+				freeRoute(rightListNode);		//free the right route
 			}
 			else
 				root->next = rightListNode;
 		}
-		*capturesOfRoute = captureRouteRight;//return the max number of capture(they are equal)
+		*capturesOfRoute = captureRouteRight;	//return the max number of capture(they are equal)
 	}
 	//if the captures on the right are more than on left
 	else if (captureRouteRight > captureRouteLeft) {
@@ -80,15 +79,15 @@ SingleSourceMovesListCell* createOptimalListFromTree(SingleSourceMovesTreeNode* 
 		freeRoute(rightListNode); //free the left route
 
 	}
-	return root;
+	return root;					//return list node
 }
-
+//This function is for updating the optimal move list tail
 SingleSourceMovesListCell* getTail(SingleSourceMovesList* lst, unsigned short captures)
 {
-	SingleSourceMovesListCell* curr;
-	curr = lst->head;
+	SingleSourceMovesListCell* curr;			//curr to move on the list
+	curr = lst->head;							//we return the head if there are no moves to do
 
-	if (captures == 0&& curr->next!=NULL) {//one move to do
+	if (captures == 0&& curr->next!=NULL) {		//one move to do
 		curr = lst->head->next;
 	}
 	//there are capture moves
@@ -97,40 +96,39 @@ SingleSourceMovesListCell* getTail(SingleSourceMovesList* lst, unsigned short ca
 		curr = curr->next;
 		captures--;
 	}
-	return curr;
+	return curr;								//return the tail
 }
-
-
-
+//This function is for creating a list of lists for each player in every single move they have
 multipleSourceMoveList* FindAllPossiblePlayerMoves(Board board, Player player)
 {
-	multipleSourceMoveList* res;
-	multipleSourceMoveListCell* tmpListCell;
-	SingleSourceMovesTree* tmpTree;
-	checkersPos* tmpPos;
-	//SingleSourceMovesList* tmpList;
+	multipleSourceMoveList* res;					//the list of lists
+	multipleSourceMoveListCell* tmpListCell;		//tmp cell
+	SingleSourceMovesTree* tmpTree;					//the single move tree
+	checkersPos* tmpPos;							//the position on board
 	int i, j;
+	//allocation for the list
 	res = (multipleSourceMoveList*)malloc(sizeof(multipleSourceMoveList));
-	checkAllocationMultipleList(res);
-	makeEmptyMultipleList(res);
-
-	for (i = 0; i < BOARD_SIZE; i++)
+	checkAllocationMultipleList(res);													//check if allocated
+	makeEmptyMultipleList(res);															//make empty list
+	//going through the board and looking for the players on board so we create the list of lists for the player
+	for (i = 0; i < BOARD_SIZE; i++)													//board rows
 	{
-		for (j = 0; j < BOARD_SIZE; j++)
+		for (j = 0; j < BOARD_SIZE; j++)												//board cols
 		{
-			if (board[i][j] == player)
+			if (board[i][j] == player)													//if we found a player on the board
 			{
-				tmpPos = createPos(i, j);
-				tmpTree = FindSingleSourceMoves(board, tmpPos);
-				tmpListCell = (multipleSourceMoveListCell*)malloc(sizeof(multipleSourceMoveListCell));
-				checkAllocationListNode(tmpListCell);
+				tmpPos = createPos(i, j);												//create the pos
+				tmpTree = FindSingleSourceMoves(board, tmpPos);							//create the tree for the pos
+				tmpListCell = (multipleSourceMoveListCell*)malloc(sizeof(multipleSourceMoveListCell));//allocating tmplist
+				checkAllocationListNode(tmpListCell);									//check if allocated
 				tmpListCell->single_source_moves_list =FindSingleSourceOptimalMove(tmpTree); //find the best route of captures from given position
-				insertListToEndMultiplefList(res, tmpListCell);
+				insertListToEndMultiplefList(res, tmpListCell);							//insert the route to the list
 			}
 		}
 	}
-	return res;
+	return res;																			//return the list of lists
 }
+//This function is for inserting the multiple list node to the end of the list of lists
 void insertListToEndMultiplefList(multipleSourceMoveList* multipleList, multipleSourceMoveListCell* newList)
 {
 	if (multipleList->head == NULL)
@@ -144,15 +142,7 @@ void insertListToEndMultiplefList(multipleSourceMoveList* multipleList, multiple
 
 	newList->next = NULL;
 }
-	//	*capturesOfRoute = captureRouteRight;
-	//
-	//}
-	//else {//if the captures on the left are more than on right
-	//	*capturesOfRoute = captureRouteLeft;
-	//	
-	//}
-//
-//}
+//This function is for creating a pos 
 checkersPos* createPos(int row, int col)
 {
 	checkersPos* pos;
@@ -166,20 +156,21 @@ checkersPos* createPos(int row, int col)
 	pos->row = CHAR_A + row;
 	return pos;
 }
-
+//This function is for inserting data to the end of a single source list
 void insertDataToEndList(SingleSourceMovesList* lst, checkersPos* pos, unsigned short captures) {
 	SingleSourceMovesListCell* newTail;
 	newTail = createNewListCell(pos,captures, NULL);
 	insertListNodeToEndList(lst, newTail);
 }
-
+//This function is for putting NULL in the head and tail for the multi list
 void makeEmptyMultipleList(multipleSourceMoveList* res) {
 	res->head = res->tail = NULL;
 }
+//This function is for putting NULL in the head and tail for the single list
 void makeEmptyList(SingleSourceMovesList* res) {
 	res->head = res->tail = NULL;
 }
-
+//This function is for checking allocation of a single source list
 void checkAllocationList(SingleSourceMovesList* res) {
 	if (res == NULL)
 	{
@@ -187,6 +178,8 @@ void checkAllocationList(SingleSourceMovesList* res) {
 		exit(1);
 	}
 }
+//This function is for checking allocation of a multi list
+
 void checkAllocationMultipleList(multipleSourceMoveList* res) {
 	if (res == NULL)
 	{
@@ -194,6 +187,7 @@ void checkAllocationMultipleList(multipleSourceMoveList* res) {
 		exit(1);
 	}
 }
+//This function is for checking allocation of a single source list cell
 void checkAllocationListNode(SingleSourceMovesListCell* res) {
 	if (res == NULL)
 	{
@@ -201,7 +195,7 @@ void checkAllocationListNode(SingleSourceMovesListCell* res) {
 		exit(1);
 	}
 }
-
+//This function is for creating a new list cell
 SingleSourceMovesListCell* createNewListCell(checkersPos* pos, unsigned short captures, SingleSourceMovesListCell* next) {
 	SingleSourceMovesListCell* res;
 	res = (SingleSourceMovesListCell*)malloc(sizeof(SingleSourceMovesListCell));
@@ -212,7 +206,7 @@ SingleSourceMovesListCell* createNewListCell(checkersPos* pos, unsigned short ca
 	res->next = next;
 	return res;
 }
-
+//This function is for inserting the list cell into the single source list
 void insertListNodeToEndList(SingleSourceMovesList* list, SingleSourceMovesListCell* res) {
 	if (isEmpty(list) == true) {
 		list->head = list->tail = res;
@@ -223,14 +217,14 @@ void insertListNodeToEndList(SingleSourceMovesList* list, SingleSourceMovesListC
 	}
 	res->next = NULL;
 }
-
+//This function is checking if the list is empty
 bool isEmpty(SingleSourceMovesList* list) {
 	if (list->head == NULL)
 		return true;
 	else
 		return false;
 }
-
+//This function is freeing the lists
 void freeRoute(SingleSourceMovesListCell* route)
 {
 	SingleSourceMovesListCell* curr, * next;
--- checkers/main.c
+++ checkers/main.c
@@ -1,56 +1,37 @@
+/*
+FINAL PROJECT IN ADAVNCED PROGRAMMING IN C 
+- CHECKERS GAME-
+Names of creators:       ID number:
+Noa Halili
+Adva Levine              319098133
+*/
 #include "main.h"
 #include "Trees.h"
 #include "Lists.h"
 
 
-//main function
+/*Checkers game!
+This program is a simple version of the game "Chcekers" we all know and love,
+the game is being played by the computer, it has a set of incoded rules it follows to accomplish its purpose.
+In this version, we have two players- Player T and Player B,
+Player T is at the top of the board, player B is at the bottom.
+each player has 12 starting tools, and in each round every player gets one turn,
+the board is being printed in each round with the update location of the tools on it and with the declartion on the move the player did
+the goal is for a player to reach the other component's side: T needs to reach row 8, and B needs to reach row 0.
+the other possibility to win is if the other player lost all its tools due to capture moves.
+if the player has a move inwhich he can get a higher number of captures, then it'll proirities it.
+else if its the same number of captures or there are no high captures and no equal captures-
+if it's player T, it'll always choose the highest row, else the highest col
+if it's player B, it'll always choose the lowest row, else the lowest col
+when the game is done, the winner is declared and its number of moves, 
+as well as the name of the player with the most captures and the amount of captures they did
+GOOD LUCK AND ENJOY! :)
+*/
 void main()
 {
     Player player;
-    player = 'B';
+    player = PLAYER_B;
     Board board;
     createBoard(board);
     PlayGame(board, player);
-    //Board board = { {0,'T',0,'T',0,'T',0,'T'},
-    //               {'T',0,'T',0,0,0,'T',0},
-    //               {0,'B',0,0,0,0,0,0},
-    //               {0,0,0,0,'B',0,0,0},
-    //               {0,0,0,0,0,0,0,'B'},
-    //               {0,0,0,0,'T',0,0,0},
-    //               {0,'B',0,0,0,0,0,'B'},
-    //               {'B',0,'B',0,'B',0,'B',0} };
-
-    //PlayGame(board, player);
-    //Turn(board, PLAYER_T);
-    //free if needed
-    //free treeNodes and free tree source-> free insdie treenode- checkerspos
 }
-    //Board board = { {0,0,0,0,0,0,0,0},
-    //                 {0,0,0,0,0,0,0,0},
-    //                 {0,0,0,'T',0,'T',0,0},
-    //                 {0,0,0,0,0,0,0,0},
-    //                 {0,0,0,'T',0,'T',0,0},
-    //                 {0,0,0,0,0,0,0,0},
-    //                 {0,0,0,'T',0,'T',0,0},
-    //                 {0,0,0,0,'B',0,0,0} };
-    //checkersPos pos = { 'H',5 }; //testing
-   
-   /* Board board = { {0,0,0,'T',0,0,0,0},
-                    {0,0,'B',0,'B',0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,'B',0,'B',0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0} };*/
-
-    //checkersPos pos = { 'H',4 }; //testing
-
-   /* Board board = { {0,0,0,'T',0,0,0,0},
-                    {0,0,'B',0,'B',0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,'B',0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0} };*/
